4691fa5e62219b28472a2970a5b17faa2e0c5b0f
Michał Sikora <me@mehow.io>
2025-02-19 20:43:07 +0100

    Highlight text when yanking

--- .config/nvim/lua/config/core/init.lua
+++ .config/nvim/lua/config/core/init.lua
@@ -12,3 +12,12 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 vim.opt.clipboard:append("unnamedplus")
+
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  desc = "Highlight text when yanking",
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+})
+
