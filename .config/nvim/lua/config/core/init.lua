145b19d08e03269e36d90a4733b31e214bf6d92d
Michał Sikora <me@mehow.io>
2025-02-19 19:41:22 +0100

    Add basic nvim config

--- .config/nvim/lua/config/core/init.lua
+++ .config/nvim/lua/config/core/init.lua
@@ -1,3 +1,14 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.expandtab = true
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+vim.opt.clipboard:append("unnamedplus")
