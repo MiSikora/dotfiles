8c5c492b143a8d127f0b1f72387a9643415924e3
Michał Sikora <me@mehow.io>
2025-02-22 20:14:30 +0100

    Add more nvim config

--- .config/nvim/lua/config/core/options.lua
+++ .config/nvim/lua/config/core/options.lua
@@ -1,14 +1,25 @@
 local opt = vim.opt
 
-opt.relativenumber = true
+opt.termguicolors = true
+opt.updatetime = 50
+opt.clipboard = "unnamedplus"
+
 opt.number = true
+opt.relativenumber = true
+opt.signcolumn = "yes"
+
 opt.wrap = false
+opt.scrolloff = 8
+opt.colorcolumn = "100"
 
-opt.shiftwidth = 2
 opt.tabstop = 2
+opt.softtabstop = 2
+opt.shiftwidth = 2
 opt.expandtab = true
+opt.autoindent = true
+opt.smartindent = true
 
+opt.hlsearch = true 
+opt.incsearch = true
 opt.ignorecase = true
 opt.smartcase = true
-
-opt.clipboard = "unnamedplus"
