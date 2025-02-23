a8d0cf656da053db3d28131903aa00492b867877
Michał Sikora <me@mehow.io>
2025-02-23 21:26:05 +0100

    Map <Space> to <Nop>

--- .config/nvim/lua/mehow/config/keymap.lua
+++ .config/nvim/lua/mehow/config/keymap.lua
@@ -3,4 +3,5 @@ vim.g.maplocalleader = "\\"
 
 local keymap = vim.keymap
 
+keymap.set("n", "<Space>", "<Nop>", { noremap = true, silent = true })
 keymap.set("n", "<Esc>", ":noh<CR>", { noremap = true, silent = true })
