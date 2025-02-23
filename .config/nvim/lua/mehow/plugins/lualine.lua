16159a50b9e393366950403a07af2b3b7ea49ad8
Michał Sikora <me@mehow.io>
2025-02-23 17:51:49 +0100

    Add nvim status line

---
+++ .config/nvim/lua/mehow/plugins/lualine.lua
@@ -0,0 +1,40 @@
+return {
+  "nvim-lualine/lualine.nvim",
+  dependecies = { "nvim-tree/nvim-web-devicons" },
+  config = function()
+    local falvour = require("catppuccin").flavour
+    local palette = require("catppuccin.palettes").get_palette(flavour)
+    local lazy_status = require("lazy.status")
+
+    require("lualine").setup {
+      options = {
+        theme = "catppuccin",
+        refresh = {
+          statusline = 8
+        },
+      },
+      sections = {
+        lualine_b = { "branch", "diagnostics" },
+        lualine_c = {
+          {
+            "filename",
+            symbols = {
+              modifed = "",
+              readonly = "",
+              unnamed = ""
+            },
+          }
+        },
+        lualine_x = {
+          {
+            lazy_status.updates,
+            cond = lazy_status.has_updates,
+            color = { fg = palette.peach },
+          },
+          { "encoding" },
+          { "filetype" },
+        },
+      },
+    }
+  end,
+}
--- .config/nvim/lua/mehow/plugins/oil.lua
+++ .config/nvim/lua/mehow/plugins/oil.lua
@@ -2,7 +2,8 @@ return {
   "stevearc/oil.nvim",
   opts = {},
   dependencies = { "nvim-tree/nvim-web-devicons" },
-  -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
+  -- Lazy loading is not recommended because it is very tricky
+  -- to make it work correctly in all situations.
   lazy = false,
   config = function()
     require("oil").setup()
