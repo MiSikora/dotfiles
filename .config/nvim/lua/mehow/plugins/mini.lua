fae8680e15427ce919b34e7f4003316115a8bd80
Michał Sikora <me@mehow.io>
2025-02-23 13:17:27 +0100

    Configure nvim mini

---
+++ .config/nvim/lua/mehow/plugins/mini.lua
@@ -0,0 +1,18 @@
+return {
+  "echasnovski/mini.nvim",
+  version = "*",
+  config = function()
+    local trailspace = require("mini.trailspace")
+    trailspace.setup()
+
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      callback = function(args)
+        trailspace.trim()
+        trailspace.trim_last_lines()
+      end,
+    })
+
+    require("mini.pairs").setup()
+    require("mini.move").setup()
+  end,
+}
