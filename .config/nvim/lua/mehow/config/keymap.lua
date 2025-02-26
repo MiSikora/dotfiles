650a29218cda3a649596e5ea512ab779b6e45722
Michał Sikora <me@mehow.io>
2025-02-26 23:57:11 +0100

    J mapping

--- .config/nvim/lua/mehow/config/keymap.lua
+++ .config/nvim/lua/mehow/config/keymap.lua
@@ -7,10 +7,13 @@ map("n", "<Esc>", "<cmd>noh<CR>", { noremap = true, silent = true })
 
 -- Remove <Space>, <Backspace>, and <Enter> navigation
 for _, key in ipairs({ "<Space>", "<BS>", "<CR>" }) do
-  map({ "n", "v" }, key, "<Nop>", { noremap = true, silent = true })
+	map({ "n", "v" }, key, "<Nop>", { noremap = true, silent = true })
 end
 
 -- Remove arrow navigation
 for _, arrow in ipairs({ "<Left>", "<Down>", "<Up>", "<Right>" }) do
-  map({ "n", "v", "i"}, arrow, "<Nop>", { noremap = true, silent = true })
+	map({ "n", "v", "i" }, arrow, "<Nop>", { noremap = true, silent = true })
 end
+
+-- Keep cursor's position
+map("n", "J", "mzJ`z")
