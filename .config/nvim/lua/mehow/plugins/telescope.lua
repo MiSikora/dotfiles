e41ea4ab259e1edd52d0e7f403653da7e66415d7
Michał Sikora <me@mehow.io>
2025-03-01 10:36:58 +0100

    Use previous telescope mapping

--- .config/nvim/lua/mehow/plugins/telescope.lua
+++ .config/nvim/lua/mehow/plugins/telescope.lua
@@ -7,7 +7,16 @@ return {
   },
   config = function()
     local telescope = require("telescope")
+    local actions = require("telescope.actions")
     telescope.setup({
+      defaults = {
+        mappings = {
+          i = {
+            ["<C-k>"] = actions.move_selection_previous,
+            ["<C-j>"] = actions.move_selection_next,
+          },
+        },
+      },
       extensions = {
         fzf = {},
       },
