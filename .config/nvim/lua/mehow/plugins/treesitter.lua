c8edd1c04f78456e2a3064b5a895d28fc3de96b4
Michał Sikora <me@mehow.io>
2025-06-09 20:37:26 +0200

    Update treesitter configuration

--- .config/nvim/lua/mehow/plugins/treesitter.lua
+++ .config/nvim/lua/mehow/plugins/treesitter.lua
@@ -1,20 +1,22 @@
-return {
+return {{
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    ---@diagnostic disable-next-line: missing-fields
     require("nvim-treesitter.configs").setup({
       ensure_installed = {
         "bash",
         "c",
         "lua",
         "vim",
+        "vimdoc",
         "query",
         "markdown",
         "markdown_inline",
         "toml",
       },
+
       auto_install = false,
+
       highlight = {
         enable = true,
         disable = function(_, buf)
@@ -26,6 +28,40 @@ return {
         end,
         additional_vim_regex_highlighting = false,
       },
+
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<Leader>ss",
+          node_incremental = "<Leader>si",
+          node_decremental = "<Leader>sd",
+          scope_incremental = false,
+        },
+      },
+  textobjects = {
+    select = {
+      enable = true,
+      lookahead = true,
+      include_surrounding_whitespace = true,
+
+      keymaps = {
+        ["af"] = { query = "@function.outer", desc = "Select around function" },
+        ["if"] = { query = "@function.inner", desc = "Select inside function" },
+        ["ac"] = { query = "@class.outer", desc = "Select around class" },
+        ["ic"] = { query = "@class.inner", desc = "Select inside class" },
+        ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select around scope" },
+      },
+
+      selection_modes = {
+        ["@parameter.outer"] = "v",
+        ["@function.outer"] = "v",
+        ["@class.outer"] = "<c-v>",
+      },
+    },
+  },
     })
   end,
+  },
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",},
 }
