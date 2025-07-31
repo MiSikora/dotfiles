cde2ac407a2592fab16e730d570bbd2a84281a52
Michał Sikora <me@mehow.io>
2025-07-31 20:45:32 +0200

    Uninstall nvim

--- .config/nvim/after/ftplugin/netrw.lua
+++
@@ -1,5 +0,0 @@
-vim.opt_local.colorcolumn = ""
-
-vim.g.netrw_banner = 0
-vim.g.netrw_bufsettings = "noma nomod nu rnu nobl nowrap ro"
-vim.g.netrw_list_hide = [[^./$,^../$]]
--- .config/nvim/init.lua
+++
@@ -1,2 +0,0 @@
-require("mehow.config")
-require("mehow.lazy")
